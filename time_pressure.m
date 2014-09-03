fe66636f46420f3ab670305af0fef97507187c32
unknown <dhli816@gmail.com>
2014-09-03 15:04:57 -0700

    This MATLAB file splits up the huge exported data textfile from labview into separate .MAT files that correspond with each escape response elicitation.

---
+++ time_pressure.m
@@ -0,0 +1,57 @@
+% Diana Li
+% loading time and voltage data into separate .mat files
+clear all; clear history; close all;clc;
+% cd('C:\Users\GILLY\Documents\MATLAB\Project\Loligo Neck Organ\')
+cd('/Volumes/THESIS/Santa Rosalia July 2014 restraining large squid/squid2')
+
+% to fetch selected file
+Filename = uigetfile('*.txt');
+[fid,message] = fopen(Filename);
+% to read the first line
+currentline = fgetl(fid);
+
+% while there are characters to read
+while ~feof(fid)
+
+    
+    % if the first character of the current line is not a number
+    if regexp(currentline(1), '\D')
+        % rename currentline to header to hold its spot
+        header = currentline;
+        
+        commas = regexp(header,',','split');
+        spaces = regexp(commas{1,2},' ','split');
+        savename = [regexprep(spaces{1,1},'/',''), '_', regexprep(spaces{1,2},':',''), spaces{1,3}];
+        %create new matrix 
+        
+        pressuredata = [];
+
+        % advance to next line
+        currentline = fgetl(fid);
+    end
+
+    % as long as the first character of the current line is a number, go.
+    while regexp(currentline(1), '\d')
+        
+        splitline = regexp(currentline,',','split');
+        
+        pressuredata = [pressuredata; ConvertLVTime(str2num(splitline{1,1})), str2num(splitline{1,2})];
+        % advance to next line
+        currentline = fgetl(fid);
+         if ~ischar(currentline(1))
+            save(strcat(savename,'.mat'),'pressuredata','-mat'); 
+
+            break
+        end
+        if regexp(currentline(1), '\D')
+         save(strcat(savename,'.mat'),'pressuredata','-mat'); 
+        end
+         
+       
+    end
+
+end
+    
+% close the file
+fclose(fid);
+
